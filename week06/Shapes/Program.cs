803fcbe6da537528ebeb05de71ff95e323ac5bb4
Valere Massango Moukouelle <m.maval6@gmail.com>
2025-02-20 19:03:02 +0100

    Done with shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+
+        // Add different shapes
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2.5));
+
+        // Iterate and display shape properties
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
